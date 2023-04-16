754a3cdf75b5c0cd06e0b62710eed4e5437dd748
ehw2133 <42625035+elliothwurst@users.noreply.github.com>
2023-04-15 23:01:32 -0700

    Created a new level design template with additional prefabs, implemented attack sets for units
    -Created a prefab for a GameObject that stores all cards that will be used in the game
    -Created a generic card prefab with all of the scripts needed for it to work; it has default values for the card type and modifier
    -Created a new level design template with the battle turn system, battle view scene, and cards
    -Updated card type names to reflect the specific names in the GDD
    -Units now have an attack set that is similar to a move set that is performed after the unit moves
    -Units can now choose to stay on their tile or not attack an opponent
    -GridNodes determine whether they are a valid attack for a unit's attack set
    -Condensed separate functions for enemy and player into one
    -Fixed a bug where a unit that had already moved was still the selected unit in the grid system

--- Cyber-Tactics/Assets/Scripts/GridNode.cs
+++ Cyber-Tactics/Assets/Scripts/GridNode.cs
@@ -7,12 +7,14 @@ public class GridNode : MonoBehaviour
     public GameObject currentUnit;      // The unit occupying this node, if any
     public Vector3 nodeWorldPos;        // The world space position of the node (Ex. Vector3(0, 1, 2)
     public Vector2 nodeGridPos;         // The grid position of the node (Ex. [0, 1] or Vector2(0, 1))
-    public bool validMove;              
+    public bool validMove;              // Whether this node is a valid move for a unit's moveset
+    public bool validAttack;            // Whether this node is a valid attack for a unit's attackset
 
     // Start is called before the first frame update
     void Start()
     {
         nodeWorldPos = transform.position;
         validMove = false;
+        validAttack = false;
     }
 }
